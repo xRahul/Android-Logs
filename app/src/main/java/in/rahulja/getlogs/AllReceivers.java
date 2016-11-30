646fe2302d54b1539cc46aed7c03cda92f030685
Rahul Jain <rahul_jain@live.in>
2016-12-01 01:13:19 +0530

    logs stored as json, added search, formatted listview

--- app/src/main/java/in/rahulja/getlogs/AllReceivers.java
+++ app/src/main/java/in/rahulja/getlogs/AllReceivers.java
@@ -1,45 +1,66 @@
 package in.rahulja.getlogs;
 
-import android.content.BroadcastReceiver;
+import android.app.admin.DeviceAdminReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.Calendar;
 
-public class AllReceivers extends BroadcastReceiver {
+public class AllReceivers extends DeviceAdminReceiver {
     public AllReceivers() {
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        String intent_string = "\n\n" + java.text.DateFormat.getDateTimeInstance()
-                                                        .format(Calendar.getInstance().getTime());
-        intent_string += "\nAction: " + intent.getAction();
-        Bundle intent_extras = intent.getExtras();
-        String extras = "";
-        if (intent_extras != null) {
-            for (String key : intent_extras.keySet()) {
-                Object value = intent_extras.get(key);
-                extras += "\t" + key + ":\t" + value.toString();
-            }
-        }
-        intent_string += "\nExtras: " + extras;
+        logAllActions(intent);
+
+    }
+
+    private void logAllActions(Intent intent) {
+        JSONObject logData = new JSONObject();
 
         try {
-            File myFile = new File(Environment.getExternalStorageDirectory(), "intentLogs.txt");
+            logData.put("datetime", java.text.DateFormat.getDateTimeInstance().format(Calendar.getInstance().getTime()));
+            logData.put("action", intent.getAction());
+
+            Bundle intent_extras = intent.getExtras();
+            if (intent_extras != null) {
+                JSONObject logExtraData = new JSONObject();
+                for (String key : intent_extras.keySet()) {
+                    Object value = intent_extras.get(key);
+                    if (value != null) {
+                        logExtraData.put(key, value.toString());
+                    }
+                }
+                logData.put("data", logExtraData);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        writeLogToFile("allLogs.txt", logData.toString());
+
+    }
+
+    private void writeLogToFile(String fileName, String data) {
+        try {
+            File myFile = new File(Environment.getExternalStorageDirectory(), fileName);
             myFile.createNewFile();
-            FileOutputStream fOut = null;
-            fOut = new FileOutputStream(myFile, true);
+            FileOutputStream fOut = new FileOutputStream(myFile, true);
             OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
-            myOutWriter.append(intent_string);
+            myOutWriter.append(data);
+            myOutWriter.append("\n");
             myOutWriter.close();
             fOut.close();
         } catch (IOException e) {
--- app/src/main/java/in/rahulja/getlogs/MainActivity.java
+++ app/src/main/java/in/rahulja/getlogs/MainActivity.java
@@ -1,47 +1,133 @@
 package in.rahulja.getlogs;
 
+import android.app.SearchManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Iterator;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    public ArrayAdapter<String> itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showLogs();
     }
 
-    public void startLogService(View view) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setSubmitButtonEnabled(true);
+        searchView.setOnQueryTextListener(this);
+
+        return true;
+    }
+
+    public void viewLogs(View view) {
+        showLogs();
+    }
+
+    private void showLogs() {
+        setLogsInListView( getLogsArray() );
+    }
+
+    private void setLogsInListView(ArrayList<String> logArray) {
+        itemsAdapter = new ArrayAdapter<String>(this, R.layout.logs_list_view_item, logArray);
+        ListView listView = (ListView) findViewById(R.id.list_view_logs);
+        if (listView != null) {
+            listView.setAdapter(itemsAdapter);
+        }
+    }
+
+    private ArrayList<String> getLogsArray() {
+
+        ArrayList<String> logArray = new ArrayList<String>();
+        File file = new File(Environment.getExternalStorageDirectory(), "allLogs.txt");
+
         try {
-            File file = new File(Environment.getExternalStorageDirectory(), "intentLogs.txt");
-
-            //Read text from file
-            StringBuilder text = new StringBuilder();
-
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
 
             while ((line = br.readLine()) != null) {
-                text.append(line);
-                text.append("\n");
+                try {
+                    JSONObject lineObj = new JSONObject(line);
+                    String toWrite = lineObj.get("datetime").toString();
+                    toWrite += "\n";
+                    toWrite += lineObj.get("action").toString();
+                    if (lineObj.has("data")) {
+                        JSONObject dataObj = lineObj.getJSONObject("data");
+                        Iterator<String> dataObjKeys = dataObj.keys();
+                        while (dataObjKeys.hasNext()) {
+                            toWrite += "\n\t\t";
+                            String key = dataObjKeys.next();
+                            String value = dataObj.get(key).toString();
+                            if (value.charAt(0) == '[') {
+                                value = value.substring(1, value.length()-1);
+                                String[] parts = value.split(", ");
+                                value = "";
+                                for (String str : parts) {
+                                    value += "\n\t\t\t\t" + str;
+                                }
+                            }
+                            if (key.equals("wifiInfo")) {
+                                String[] parts = value.split(", ");
+                                value = "";
+                                for (String str : parts) {
+                                    value += "\n\t\t\t\t" + str;
+                                }
+                            }
+                            toWrite += key + ": " + value;
+                        }
+                    }
+                    logArray.add(0, toWrite);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
             br.close();
 
-            TextView tv = (TextView)findViewById(R.id.logs_text_view);
-            tv.setText(text.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return logArray;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        itemsAdapter.getFilter().filter(newText);
+        return true;
     }
 }
